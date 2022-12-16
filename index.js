311d2ccd4f781148c83b5e87b5312d60983982e6
YEAHx4 <developerstarlight@gmail.com>
2022-12-16 16:25:51 +0900

    bug fix and intergeration

--- index.js
+++ index.js
@@ -5,8 +5,18 @@ let fieldSet;
 let labels;
 let cma;
 let flipper;
+let ls;
 let running = false;
 let flip = true; // TODO 조절할수 있게
+
+const rankings = (array) => {
+  return array
+    .map((v, i) => [v, i])
+    .sort((a, b) => b[0] - a[0])
+    .map((a, i) => [...a, i + 1])
+    .sort((a, b) => a[1] - b[1])
+    .map((a) => a[2]);
+};
 
 const stop = () => {
   running = false;
@@ -16,6 +26,7 @@ const stop = () => {
   fieldSet.disabled = false;
   labels.innerHTML = "";
   cam.innerHTML = "";
+  labels.style.visibility = "hidden";
 };
 
 const start = async () => {
@@ -36,21 +47,32 @@ const start = async () => {
   webcam = new tmImage.Webcam(550, 550, flip);
   await webcam.setup();
   await webcam.play();
+  labels.style.visibility = "visible";
 
   const predict = async () => {
     const prediction = await model.predict(webcam.canvas);
 
     const probs = prediction.map((v) => parseFloat(v.probability.toFixed(2)));
-    const max = probs.indexOf(Math.max(...probs));
+    const ranks = rankings(probs);
 
-    for (let i = 0; i < maxPredictions; i++) {
-      const classPrediction =
-        prediction[i].className + ": " + probs[i] * 100 + "%";
-      labelContainer.childNodes[i].innerHTML = classPrediction;
+    ls[0].innerHTML = `${prediction[ranks.indexOf(1)].className}: ${Math.floor(
+      probs[ranks.indexOf(1)] * 100
+    )}%`;
+    ls[1].innerHTML = `${prediction[ranks.indexOf(2)].className}: ${Math.floor(
+      probs[ranks.indexOf(2)] * 100
+    )}%`;
+    ls[2].innerHTML = `${prediction[ranks.indexOf(3)].className}: ${Math.floor(
+      probs[ranks.indexOf(3)] * 100
+    )}%`;
 
-      if (i === max) labelContainer.childNodes[i].classList.add("max");
-      else labelContainer.childNodes[i].classList.remove("max");
-    }
+    // for (let i = 0; i < maxPredictions; i++) {
+    //   const classPrediction =
+    //     prediction[i].className + ": " + Math.floor(probs[i] * 100) + "%";
+    //   labelContainer.childNodes[i].innerHTML = classPrediction;
+
+    //   if (i === max) labelContainer.childNodes[i].classList.add("max");
+    //   else labelContainer.childNodes[i].classList.remove("max");
+    // }
   };
 
   const loop = async () => {
@@ -68,9 +90,9 @@ const start = async () => {
 
   document.getElementById("webcam-container").appendChild(webcam.canvas);
   labelContainer = document.getElementById("label-container");
-  for (let i = 0; i < maxPredictions; i++) {
-    labelContainer.appendChild(document.createElement("div"));
-  }
+  // for (let i = 0; i < maxPredictions; i++) {
+  // labelContainer.appendChild(document.createElement("div"));
+  // }
 };
 
 startBtn = document.querySelector("button#start");
@@ -78,5 +100,11 @@ fieldSet = document.querySelector("fieldset");
 labels = document.querySelector("#label-container");
 cam = document.querySelector("#webcam-container");
 flipper = document.querySelector("#flip");
+ls = [
+  document.querySelector("#l1"),
+  document.querySelector("#l2"),
+  document.querySelector("#l3"),
+];
 
 startBtn.addEventListener("click", start);
+labels.style.visibility = "hidden";
