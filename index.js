8a00266bdccef30c8b3a504d3bf3f0a318f5bf39
YEAHx4 <developerstarlight@gmail.com>
2022-12-16 13:44:51 +0900

    show webcam and predict

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
 const URL = "http://localhost:9000/";
 
-const start = () => {
-  console.log(document.querySelector('input[type="radio"]:checked').value);
+const start = async () => {
+  const group =
+    document.querySelector('input[type="radio"]:checked').value + "/";
+  const modelURL = URL + group + "model.json";
+  const metadataURL = URL + group + "metadata.json";
+
+  model = await tmImage.load(modelURL, metadataURL);
+  maxPredictions = model.getTotalClasses();
+
+  const flip = true; // TODO 조절할수 있게
+  webcam = new tmImage.Webcam(512, 512, flip);
+  await webcam.setup();
+  await webcam.play();
+
+  const predict = async () => {
+    const prediction = await model.predict(webcam.canvas);
+    for (let i = 0; i < maxPredictions; i++) {
+      const classPrediction =
+        prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+      labelContainer.childNodes[i].innerHTML = classPrediction;
+    }
+  };
+
+  const loop = async () => {
+    webcam.update();
+    await predict();
+    window.requestAnimationFrame(loop);
+  };
+
+  window.requestAnimationFrame(loop);
+
+  document.getElementById("webcam-container").appendChild(webcam.canvas);
+  labelContainer = document.getElementById("label-container");
+  for (let i = 0; i < maxPredictions; i++) {
+    labelContainer.appendChild(document.createElement("div"));
+  }
 };
 
 document.querySelector("button#start").addEventListener("click", start);
