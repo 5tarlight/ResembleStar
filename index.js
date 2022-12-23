a844564e48797033602d7ef2f42dcd30514ccb8e
YEAHx4 <developerstarlight@gmail.com>
2022-12-23 10:44:34 +0900

    bug fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const URL = "http://localhost:9000/";
 let startBtn;
 let fieldSet;
 let labels;
-let cma;
+let cam;
 let flipper;
 let ls;
 let loading;
@@ -146,100 +146,100 @@ fieldSet.onchange = (e) => {
 };
 
 particlesJS("particles-js", {
-  "particles": {
-    "number": {
-      "value": 400,
-      "density": {
-        "enable": true,
-        "value_area": 800
-      }
-    },
-    "color": {
-      "value": "#fff"
-    },
-    "shape": {
-      "type": "circle",
-      "stroke": {
-        "width": 0,
-        "color": "#000000"
-      },
-      "polygon": {
-        "nb_sides": 5
+  particles: {
+    number: {
+      value: 400,
+      density: {
+        enable: true,
+        value_area: 800,
       },
     },
-    "opacity": {
-      "value": 1,
-      "random": false,
-      "anim": {
-        "enable": false,
-        "speed": 1,
-        "opacity_min": 0.1,
-        "sync": false
-      }
+    color: {
+      value: "#fff",
     },
-    "size": {
-      "value": 10,
-      "random": true,
-      "anim": {
-        "enable": false,
-      }
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0,
+        color: "#000000",
+      },
+      polygon: {
+        nb_sides: 5,
+      },
     },
-    "line_linked": {
-      "enable": false,
+    opacity: {
+      value: 1,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false,
+      },
     },
-    "move": {
-      "enable": true,
-      "speed": 6,
-      "direction": "bottom",
-      "random": false,
-      "straight": false,
-      "out_mode": "out",
-      "bounce": false,
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 1200
-      }
-    }
+    size: {
+      value: 10,
+      random: true,
+      anim: {
+        enable: false,
+      },
+    },
+    line_linked: {
+      enable: false,
+    },
+    move: {
+      enable: true,
+      speed: 6,
+      direction: "bottom",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200,
+      },
+    },
   },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": true,
-        "mode": "repulse"
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse",
       },
-      "onclick": {
-        "enable": true,
-        "mode": "repulse"
+      onclick: {
+        enable: true,
+        mode: "repulse",
       },
-      "resize": true
+      resize: true,
     },
-    "modes": {
-      "grab": {
-        "distance": 400,
-        "line_linked": {
-          "opacity": 0.5
-        }
+    modes: {
+      grab: {
+        distance: 400,
+        line_linked: {
+          opacity: 0.5,
+        },
       },
-      "bubble": {
-        "distance": 100,
-        "size": 4,
-        "duration": 0.3,
-        "opacity": 1,
-        "speed": 3
+      bubble: {
+        distance: 100,
+        size: 4,
+        duration: 0.3,
+        opacity: 1,
+        speed: 3,
       },
-      "repulse": {
-        "distance": 100,
-        "duration": 0.4
+      repulse: {
+        distance: 100,
+        duration: 0.4,
       },
-      "push": {
-        "particles_nb": 4
+      push: {
+        particles_nb: 4,
       },
-      "remove": {
-        "particles_nb": 2
-      }
-    }
+      remove: {
+        particles_nb: 2,
+      },
+    },
   },
-  "retina_detect": true
+  retina_detect: true,
 });
